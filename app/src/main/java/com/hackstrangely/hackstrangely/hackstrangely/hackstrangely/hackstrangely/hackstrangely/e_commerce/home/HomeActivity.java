ef293c3e8a517d80f3408564463ede6c1df7ec28
Vrihas123 <vrihas007@gmail.com>
2018-02-04 15:37:20 +0530

    final push hackathon

--- app/src/main/java/com/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/e_commerce/home/HomeActivity.java
+++ app/src/main/java/com/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/e_commerce/home/HomeActivity.java
@@ -11,11 +11,14 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.e_commerce.R;
 import com.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.e_commerce.calculator.view.CalculatorFragment;
 
+import com.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.e_commerce.disease.view.DiseaseFragment;
 import com.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.e_commerce.products.view.ProductFragment;
 import com.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.e_commerce.schemes.view.SchemesFragment;
 
@@ -42,6 +45,10 @@ public class HomeActivity extends AppCompatActivity {
     CardView card3;
     @BindView(R.id.card4_home)
     CardView card4;
+    @BindView(R.id.card5_home)
+    CardView card5;
+    @BindView(R.id.card6_home)
+    CardView card6;
 
     @BindView(R.id.home_item_name1)
     TextView item1;
@@ -51,13 +58,30 @@ public class HomeActivity extends AppCompatActivity {
     TextView item3;
     @BindView(R.id.home_item_name4)
     TextView item4;
+    @BindView(R.id.home_item_name5)
+    TextView item5;
+    @BindView(R.id.home_item_name6)
+    TextView item6;
+
+    @BindView(R.id.item1_img)
+    ImageView img1;
+    @BindView(R.id.item2_img)
+    ImageView img2;
+    @BindView(R.id.item3_img)
+    ImageView img3;
+    @BindView(R.id.item4_img)
+    ImageView img4;
+    @BindView(R.id.item5_img)
+    ImageView img5;
+    @BindView(R.id.item6_img)
+    ImageView img6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
-        setFragment(new CalculatorFragment());
+        //setFragment(new CalculatorFragment());
 
 
         card1.setOnClickListener(new View.OnClickListener() {
@@ -81,12 +105,16 @@ public class HomeActivity extends AppCompatActivity {
         card4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-
-
+                addFragment(new DiseaseFragment());
             }
         });
+        Glide.with(this).load("http://www.asianentrepreneur.org/wp-content/uploads/2014/08/Agricultural-India.jpg").into(img1);
+        Glide.with(this).load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_0dP1tu3-C01x-TcGxKrorC5t7Bf5fz3rDtKQvZzLO0QfmKw6").into(img2);
+        Glide.with(this).load("https://www.unionbankofindia.co.in/images/RURALAGRI.jpg").into(img3);
+        Glide.with(this).load("http://ipm.uconn.edu/documents/raw2/html/images/clip_image002_071.jpg").into(img4);
+        Glide.with(this).load("http://www.jayalaxmiagrotech.com/images/TimesofIndiaHubballiEdition02102015.jpg").into(img5);
+        Glide.with(this).load("https://comtechies.com/wp-content/uploads/2017/07/discussion-forum-like-stackoverflow.jpg").into(img6);
+
 //        //setFragment(new HomeFragment());
 //
 //        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_home);
--- app/src/main/java/com/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/e_commerce/splash_screen/SplashScreenActivity.java
+++ app/src/main/java/com/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/e_commerce/splash_screen/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.e_commerce.splash_screen;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +14,7 @@ import com.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely
 import com.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.e_commerce.login.view.LoginActivity;
 import com.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.e_commerce.register.view.RegisterActivity;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends Activity {
 
     private SharedPrefs sharedPrefs;
     private ImageView logo_img;
@@ -24,18 +25,24 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         sharedPrefs = new SharedPrefs(this);
         logo_img = (ImageView) findViewById(R.id.logo);
-        Glide.with(this).load("https://i.pinimg.com/originals/d5/ca/f1/d5caf1736960cc9ff32692faff0ccb8b.png").into(logo_img);
+        Glide.with(this).load(R.drawable.logo_ks).into(logo_img);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 if (!sharedPrefs.isRegistered()){
                     Intent i =new Intent(SplashScreenActivity.this, RegisterActivity.class);
+                    startActivity(i);
+                    finish();
                 }
                 else if (!sharedPrefs.isLoggedIn()){
                     Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    finish();
                 }else {
                     Intent i =new Intent(SplashScreenActivity.this,LanguageActivity.class);
+                    startActivity(i);
+                    finish();
                 }
             }
         },3000);
